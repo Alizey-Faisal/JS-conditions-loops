391ee3f6187589d706ce2ce7a1fa20d38b2a7987
lab5pc12 <lab5pc12@tester.com>
2025-03-22 10:04:01 +0500

    javascript added

---
+++ java.js
@@ -0,0 +1,17 @@
+let user = prompt("enter the table number")
+let start = prompt("enter the starting number")
+let end = prompt("enter the ending number")
+
+for (let i = start;i <= end; i++){
+console.log(`${user} * ${i} = ${user*i}`);
+}
+
+// ternary operator|condition
+
+let age = 28
+console.log(age >= 18? "true":"false");
+
+// Array
+
+let fruits = ["leechi","banana","melon","kiwi","chikoo","gooseberry","apple"]
+console.log(fruits)
